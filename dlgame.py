c65770164abff8346b8ebc4b7a9005e72cb9e325
shangeth <shangethrajaa@gmail.com>
2018-09-17 01:47:48 +0530

    lane detection

--- dlgame.py
+++ dlgame.py
@@ -13,12 +13,25 @@ def roi(img, vertices):
     masked = cv2.bitwise_and(img, mask)
     return masked
 
+def draw_lines(img, lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
+    except:
+        pass
+
 
 def process_img(orig_img):
     processed_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    processed_img = cv2.GaussianBlur(processed_img, (5,5), 0)
     vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]])
     processed_img = roi(processed_img, [vertices])
+
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 200, 3)
+    draw_lines(processed_img, lines)
+
     return processed_img
 
 
