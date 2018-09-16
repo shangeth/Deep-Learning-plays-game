b091d243e6ef90bae0e8013ea5015850af85bf22
shangeth <shangethrajaa@gmail.com>
2018-09-17 02:30:10 +0530

    slopes of lane

--- dlgame.py
+++ dlgame.py
@@ -5,7 +5,9 @@ from mss import mss
 from PIL import Image
 from pykeyboard import PyKeyboard
 import pyautogui
-
+from numpy import ones,vstack
+from numpy.linalg import lstsq
+from statistics import mean
 
 def roi(img, vertices):
     mask = np.zeros_like(img)
@@ -13,13 +15,98 @@ def roi(img, vertices):
     masked = cv2.bitwise_and(img, mask)
     return masked
 
-def draw_lines(img, lines):
-    try:
-        for line in lines:
-            coords = line[0]
-            cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
-    except:
-        pass
+# def draw_lines(img, lines):
+#     try:
+#         for line in lines:
+#             coords = line[0]
+#             cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
+#     except:
+#         pass
+
+
+
+def draw_lines(img, lines, color=[0, 255, 255], thickness=3):
+
+    if lines is not None:
+        ys = []  
+        for i in lines:
+            for ii in i:
+                ys += [ii[1],ii[3]]
+        min_y = min(ys)
+        max_y = 600
+        new_lines = []
+        line_dict = {}
+
+        for idx,i in enumerate(lines):
+            for xyxy in i:
+               x_coords = (xyxy[0],xyxy[2])
+                y_coords = (xyxy[1],xyxy[3])
+                A = vstack([x_coords,ones(len(x_coords))]).T
+                m, b = lstsq(A, y_coords)[0]
+
+    
+                x1 = (min_y-b) / m
+                x2 = (max_y-b) / m
+
+                line_dict[idx] = [m,b,[int(x1), min_y, int(x2), max_y]]
+                new_lines.append([int(x1), min_y, int(x2), max_y])
+
+        final_lanes = {}
+
+        for idx in line_dict:
+            final_lanes_copy = final_lanes.copy()
+            m = line_dict[idx][0]
+            b = line_dict[idx][1]
+            line = line_dict[idx][2]
+            
+            if len(final_lanes) == 0:
+                final_lanes[m] = [ [m,b,line] ]
+                
+            else:
+                found_copy = False
+
+                for other_ms in final_lanes_copy:
+
+                    if not found_copy:
+                        if abs(other_ms*1.2) > abs(m) > abs(other_ms*0.8):
+                            if abs(final_lanes_copy[other_ms][0][1]*1.2) > abs(b) > abs(final_lanes_copy[other_ms][0][1]*0.8):
+                                final_lanes[other_ms].append([m,b,line])
+                                found_copy = True
+                                break
+                        else:
+                            final_lanes[m] = [ [m,b,line] ]
+
+        line_counter = {}
+
+        for lanes in final_lanes:
+            line_counter[lanes] = len(final_lanes[lanes])
+
+        top_lanes = sorted(line_counter.items(), key=lambda item: item[1])[::-1][:2]
+
+        lane1_id = top_lanes[0][0]
+        lane2_id = top_lanes[1][0]
+
+        def average_lane(lane_data):
+            x1s = []
+            y1s = []
+            x2s = []
+            y2s = []
+            for data in lane_data:
+                x1s.append(data[2][0])
+                y1s.append(data[2][1])
+                x2s.append(data[2][2])
+                y2s.append(data[2][3])
+            return int(mean(x1s)), int(mean(y1s)), int(mean(x2s)), int(mean(y2s)) 
+
+        l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
+        l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
+
+        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2]
+
+
+
+
+
 
 
 def process_img(orig_img):
@@ -29,7 +116,7 @@ def process_img(orig_img):
     vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]])
     processed_img = roi(processed_img, [vertices])
 
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 200, 3)
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 100, 5)
     draw_lines(processed_img, lines)
 
     return processed_img
@@ -45,7 +132,7 @@ while True:
     t = time.time()
     sct.get_pixels(box)
     image = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
-    image=np.array(image)
+    image=np.array(image) 
     new_screen = process_img(image)
 
     # cv2.imshow('test', image)
